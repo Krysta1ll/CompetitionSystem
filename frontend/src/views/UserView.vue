c1384b8ebcc44c3f831926471e626443a6e9b579
Krystal <1760739491@qq.com>
2023-12-04 16:36:00 +0800

    美化了用户界面，加入了云图功能

--- frontend/src/views/UserView.vue
+++ frontend/src/views/UserView.vue
@@ -1,25 +1,26 @@
 <template>
+    <header class="header">
+        <h1>竞争性关键词评分系统</h1>
+
+        <el-button class="logout-button" @click="logout">退出登录</el-button>
+        <!-- 移动搜索栏到框体外部 -->
+        <el-input
+            placeholder="请输入关键词"
+            v-model="searchQuery"
+            class="search-input"
+            @keyup.enter="fetchKeywords"
+        >
+            <template #append>
+                <el-button icon="el-icon-search" @click="fetchKeywords">搜索</el-button>
+            </template>
+        </el-input>
+    </header>
     <div class="user-keywords">
         <!-- 居中放置框体 -->
 
         <div class="center-container">
-            <header>
-                <h1 style="align-items: center">竞争性关键词评分系统</h1>
-            </header>
-            <el-button class="logout-button" @click="logout">退出登录</el-button>
-            <!-- 移动搜索栏到框体外部 -->
-            <el-input
-                placeholder="请输入关键词"
-                v-model="searchQuery"
-                class="search-input"
-                @keyup.enter="fetchKeywords"
-            >
-                <template #append>
-                    <el-button icon="el-icon-search" @click="fetchKeywords">搜索</el-button>
-                </template>
-            </el-input>
 
-            <!-- 添加框体，并调整内部样式 -->
+
             <div class="glass-box">
                 <p>搜索结果</p>
                 <div v-if="keywords.length > 0" class="keywords-list">
@@ -27,36 +28,44 @@
                         v-for="(keyword, index) in keywords"
                         :key="index"
                         class="keyword-item"
-                        @mouseenter="showCompetitiveness(keyword.comp)"
-                        @mouseleave="hideCompetitiveness()"
                     >
                         <span>{{ keyword.k }}</span>
-                        <el-rate v-model="ratings[index]" show-text @change="rateKeyword(index)"></el-rate>
+                        <span class="competitiveness">竞争度: {{ keyword.comp }}</span>
+                        <el-tooltip class="item" effect="dark" placement="top" :content="'评分: ' + ratings[index]">
+                            <el-rate v-model="ratings[index]"  @change="rateKeyword(index)"></el-rate>
+                        </el-tooltip>
                     </div>
                 </div>
-                <!-- 显示竞争度 -->
-                <div v-if="showCompetitive" class="competitive-info">
-                    竞争度: {{ currentCompetitiveness }}
-                </div>
+            </div>
+
+            <div class="glass-box">
+                <el-button @click="generateWordCloud">生成云图</el-button>
+                <p>云图</p>
+                <div ref="wordCloud" class="echarts-word-cloud"></div>
+                <!-- 在这里放置您的云图组件 -->
+
             </div>
         </div>
 
     </div>
+
+
 </template>
 
 <script>
 import axios from "axios";
-import index from "@/views/Index.vue";
-import VueWordCloud from "vue-wordcloud";
+import * as echarts from 'echarts/core';
+
+import { CanvasRenderer } from 'echarts/renderers';
+
+// 注册词云图和渲染器
+echarts.use([CanvasRenderer]);
+
 export default {
     components: {
-        VueWordCloud,
+        // 您的组件
     },
-    computed: {
-        index() {
-            return index
-        }
-    },
+
     data() {
         return {
             searchQuery: "",
@@ -67,53 +76,72 @@ export default {
             email: "",
             showCompetitive: false, // 是否显示竞争度
             currentCompetitiveness: "", // 当前关键词的竞争度
-            wordCloudData: [
-                { text: "词汇1", value: 10 },
-                { text: "词汇2", value: 8 },
-                { text: "词汇3", value: 6 },
-                { text: "词汇4", value: 4 },
-                { text: "词汇5", value: 2 },
-            ],
         };
     },
+
+    computed: {
+
+    },
+
     created() {
         if (!localStorage.getItem("userToken_user")) this.$router.push("/");
         const token = localStorage.getItem('userToken_user');
-        console.log(token);
         axios.defaults.headers.common['token'] = `${token}`;
     },
+
+
+
     methods: {
-        showCompetitiveness(competitiveness) {
-            this.currentCompetitiveness = competitiveness;
-            this.showCompetitive = true;
-        },
-
-        // 鼠标移出时隐藏竞争度
-        hideCompetitiveness() {
-            this.showCompetitive = false;
-        },
-
         logout() {
-            // 移除JWT Token
             localStorage.removeItem('userToken_user');
-            // 清空用户信息（如果有的话）
             this.user = '';
             this.username = '';
             this.email = '';
-            // 重定向到登录页面
             this.$router.push("/");
+        },
+        generateWordCloud() {
+            // 使用 ref 获取 wordCloud 元素
+            const chart = echarts.init(this.$refs.wordCloud);
+
+            // 从 this.keywords 中获取数据，转换为云图数据格式
+            const wordCloudData = this.keywords.map(keyword => ({
+                name: keyword.k,
+                value: keyword.comp
+            }));
+
+            // 设置云图的配置项
+            const option = {
+                series: [{
+                    type: 'wordCloud',
+                    gridSize: 2,
+                    sizeRange: [12, 50],
+                    rotationRange: [-90, 90],
+                    shape: 'pentagon',
+                    width: '100%',
+                    height: '100%',
+                    textStyle: {
+                        normal: {
+                            color: () => {
+                                return 'rgb(' + [
+                                    Math.round(Math.random() * 255),
+                                    Math.round(Math.random() * 255),
+                                    Math.round(Math.random() * 255)
+                                ].join(',') + ')';
+                            }
+                        }
+                    },
+                    data: wordCloudData
+                }]
+            };
+
+            // 使用 setOption 方法设置云图配置
+            chart.setOption(option);
         },
         fetchKeywords() {
             axios.get(`/models/${this.searchQuery}`)
                 .then(response => {
-                    // 清空原有数据
-                    this.keywords = [];
-                    this.ratings = [];
-                    // 添加搜索结果
                     this.keywords = response.data.data;
-                    // 初始化评分数组
                     this.ratings = new Array(this.keywords.length).fill(0);
-                    console.log(response.data.data);
                 })
                 .catch(error => {
                     console.error('搜索失败', error);
@@ -121,30 +149,23 @@ export default {
                 });
         },
         rateKeyword(index) {
-            // 发送评分到后端或进行其他操作
             const keyword = this.keywords[index];
             const rating = this.ratings[index];
-            // 可以在这里将评分信息发送到后端保存，示例代码如下：
-            // axios.post('/ratings', { keywordId: keyword.id, rating: rating })
-            //     .then(response => {
-            //         this.$message.success('评分成功');
-            //     })
-            //     .catch(error => {
-            //         console.error('评分失败', error);
-            //         this.$message.error('评分过程中发生错误');
-            //     });
+            // 发送评分到后端的逻辑
         },
     }
 }
 </script>
+
 <style>
 
 .center-container {
     display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh; /* 让内容垂直居中 */
+    flex-direction: row; /* 水平排列子元素 */
+    align-items: flex-start;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    height: auto; /* 自动高度 */
 }
 
 .search-input {
@@ -158,8 +179,8 @@ export default {
     border-radius: 10px;
     padding: 20px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-    width: 400px;
     height: 300px;
+    width: 45%;
     max-width: 400px;
     color: #555; /* 修改字体颜色 */
     display: flex;
@@ -167,14 +188,23 @@ export default {
     align-items: center;
     background: rgba(255, 255, 255, 0.7);
     background: linear-gradient(to right, #a1c4fd 0%, #c2e9fb 100%);
-}
+    flex: 1;
+    margin: 10px;
 
+}
 .keyword-item {
     display: flex;
     align-items: center;
+    justify-content: space-between; /* 调整为平均分布 */
     margin-bottom: 10px;
-    font-size: 18px; /* 调整字体大小 */
-    color: #333; /* 修改字体颜色 */
+    font-size: 18px;
+    color: #333;
+}
+
+.competitiveness {
+    margin-left: 10px;
+    color: #666;
+    font-size: 15px;
 }
 
 .logout-button {
@@ -189,12 +219,15 @@ export default {
 .logout-button:hover {
     background-color: #ff7875; /* 深一点的红色 */
 }
-.competitive-info {
-    margin-top: 5px;
-    font-size: 14px;
-    color: #555;
+.header {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    display: flex;
 }
-.wordcloud-container {
-     margin-top: 20px;
- }
+
+.echarts-word-cloud {
+    width: 100%;
+    height: 400px;
+}
 </style>
